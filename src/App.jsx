74a58263fa0b772cf8739d6f2cbd85c72c268362
Creewie <tabero2006@gmail.com>
2024-02-12 19:52:21 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  var pytania = ['que']
+
+  return (
+    <>
+      <div>
+        <h1>Gra: Q&A</h1>
+        <p>Pytanie</p>
+        <input type='text' id='odpowiedz' placeholder='Tu wpisz odpowiedź'></input>
+        <p>Punkty</p>
+        <button id='rozpocznij'>Rozpocznij gre</button>
+        <p>Zakończenie gry</p>
+        <button id='zrestartuj'>Zrestartuj gre</button>
+      </div>
+    </>
+  )
+}
+
+export default App
